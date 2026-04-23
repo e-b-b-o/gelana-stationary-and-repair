8011a6219cf92bb9d3ae6b9a37d79ee0fc478a3c
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-04-23 17:51:15 +0300

    Added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,28 +1,26 @@
 import Button from "./Button";
+import { formatCurrency } from "../utils/formatCurrency";
+import { StarIcon } from "@heroicons/react/16/solid";
 
 function ProductCard({ product }) {
   return (
-    <div className="space-y-3">
+    <div className="space-y-3 p-4 rounded-sm shadow-sm hover:shadow-lg transition-all duration-300 hover:-translate-y-1">
       <img
         src={product.image}
         alt="product image"
-        className="h-60 w-auto object-cover rounded-sm"
+        className="h-60 w-full object-cover rounded-sm"
       />
-      <div className="flex justify-between">
-        <div>
-          <h3 className="font-bold text-md">{product.name}</h3>
-          <p className="font-semibold text-sm">{product.category}</p>
-        </div>
-        <div>
-          <p className="text-muted text-sm">{product.price}</p>
-          <span className="font-semibold text-sm">{product.rating}</span>
-        </div>
+      <div>
+        <h3 className="font-bold text-base">{product.name}</h3>
+        <p className="text-muted text-xs uppercase">{product.category}</p>
+        <p className="font-bold text-base">{formatCurrency(product.price)}</p>
+        <span className="font-semibold text-sm">
+          {product.rating}
+          <StarIcon className="h-4.5 text-amber-500 inline ml-1 " />
+        </span>
       </div>
-      <div className="flex gap-2">
+      <div className="w-full">
         <Button variant="primary" size="sm">
-          Buy
-        </Button>
-        <Button variant="outline" size="sm">
           Add to cart
         </Button>
       </div>
