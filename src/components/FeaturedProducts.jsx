b166e5cd56a6876cd9a33daff7f53ac3c7154f79
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-04-23 17:50:55 +0300

    Added

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -2,18 +2,18 @@ import products from "../data/products";
 import ProductCard from "./ProductCard";
 
 function FeaturedProducts() {
-  const featuredProducts = products.filter((products) => products.featured);
+  const featuredProducts = products.filter((product) => product.featured);
   const limitedProducts = featuredProducts.slice(0, 4);
 
   return (
-    <section className="space-y-3 max-w-6xl">
+    <section className="space-y-8 max-w-6xl mx-auto py-16">
       <div className="text-center">
         <h2 className="text-2xl sm:text-3xl font-bold">Featured Products</h2>
-        <p className="text-md text-muted ">
+        <p className="text-sm md:text-base text-muted ">
           Best selling products at our store
         </p>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {limitedProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
