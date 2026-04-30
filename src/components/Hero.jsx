403d733854d7936a158bd36f202424c7c4ed0557
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-04-30 23:48:47 +0300

    finished Hero for nw

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
           backgroundImage: "url(/Gemini_Generated_Image_fgzpy2fgzpy2fgzp.png)",
         }}
       />
-      <div className="absolute inset-0 bg-black/20 rounded-xl" />
+      <div className="absolute inset-0 bg-black/50 rounded-xl" />
       <div className="relative z-10 max-w-6xl mx-auto md:mx-0 px-4 text-white md:text-left text-center">
         <div className="max-w-xl">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold leading-tight">
