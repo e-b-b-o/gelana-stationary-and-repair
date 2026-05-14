4d6113943636e95f2a1aa7a9589578959736f54c
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-05-14 15:12:36 +0300

    fix: adjust padding in MobileMenu component for improved layout

--- src/ui/MobileMenu.jsx
+++ src/ui/MobileMenu.jsx
@@ -6,7 +6,7 @@ import { XMarkIcon } from "@heroicons/react/16/solid";
 function MobileMenu({ setIsOpen, isOpen }) {
   return (
     <aside
-      className={`sm:hidden flex flex-col fixed top-0 right-0 h-full w-full bg-white shadow-lg transform transition-transform duration-300 z-50 p-4 pt-10 gap-2 ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+      className={`sm:hidden flex flex-col fixed top-0 right-0 h-full w-full bg-white shadow-lg transform transition-transform duration-300 z-50 p-4 pt-8 gap-2 ${isOpen ? "translate-x-0" : "translate-x-full"}`}
     >
       <div className="flex justify-between">
         <Input />
