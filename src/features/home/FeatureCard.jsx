7278c2f402f4b4866dcc2ca982c7d5ee564c78cd
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-05-18 23:04:20 +0300

    fix: update background color in FeatureCard component for better visibility

--- src/features/home/FeatureCard.jsx
+++ src/features/home/FeatureCard.jsx
@@ -1,6 +1,6 @@
 function FeatureCard({ icon, title, description }) {
   return (
-    <div className="text-center space-y-3 bg-muted/20 p-6  shadow-sm hover:shadow-md hover:-translate-y-1 transition-all duration-300 h-full flex flex-col ">
+    <div className="text-center space-y-3 bg-white p-6  shadow-sm hover:shadow-md hover:-translate-y-1 transition-all duration-300 h-full flex flex-col ">
       <div className="flex justify-center ">{icon}</div>
 
       <h3 className="text-lg font-semibold">{title}</h3>
