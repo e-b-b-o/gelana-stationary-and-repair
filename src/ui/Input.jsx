bd8a4faf2c6e03229bd8c9c06671ecdd2b549ebc
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-05-19 22:35:41 +0300

    feat: enhance Input component with dynamic props for placeholder, classname, variant, and size

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,15 +1,26 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
-function Input() {
+function Input({ placeholder, classname, variant, size }) {
+  const base =
+    "w-full outline-0 pl-7 pr-2 py-1 text-sm border border-gray-300 rounded-md";
+
+  const variants = {
+    primary: "bg-primary text-white hover:opacity-80 border-primary border-2",
+    search:
+      "border-primary text-primary  border-2  hover:bg-primary hover:text-white  hover:border-primary ",
+  };
+  const sizes = {
+    xs: "px-2 py-0.5 text-xs",
+    sm: "px-3 py-1 text-sm",
+    md: "px-4 py-2 text-sm",
+    lg: "px-6 py-3 text-base",
+  };
   return (
-    <div className="relative flex items-center min-w-0 w-full max-w-[160px] lg:max-w-[220px]">
-      <MagnifyingGlassIcon className="w-4 absolute left-2 shrink-0" />
-      <input
-        type="text"
-        placeholder="Search..."
-        className="w-full outline-0 pl-7 pr-2 py-1 text-sm border border-gray-300 rounded-md"
-      />
-    </div>
+    <input
+      type="text"
+      placeholder={placeholder}
+      className={base + variants[variant] + sizes[size] + classname}
+    />
   );
 }
 
