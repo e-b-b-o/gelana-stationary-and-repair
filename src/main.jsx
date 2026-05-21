8678f74c4c3efddb36b1530a28a31eb3e50bf5e6
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-05-21 23:11:47 +0300

    feat: wrap RouterProvider with AuthProvider for user authentication context

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,12 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
 import App from "./app/App";
 
+import { AuthProvider } from "./features/auth/AuthContext";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
