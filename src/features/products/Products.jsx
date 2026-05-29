8aa479b9b0fee8786213d40733a8002c0e4c6ae6
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-05-29 22:06:10 +0300

    feat: implement product filtering based on search parameters and category

--- src/features/products/Products.jsx
+++ src/features/products/Products.jsx
@@ -1,9 +1,23 @@
+import { useSearchParams } from "react-router-dom";
 import products from "../../data/products";
 import Button from "../../ui/Button";
 import Input from "../../ui/Input";
 import ProductCard from "./ProductCard";
+import ProductFilters from "./ProductFilters";
 
 function Products() {
+  const [searchParams] = useSearchParams();
+  const q = searchParams.get("q")?.toLowerCase() || "";
+  const category = searchParams.get("category") || "all";
+
+  // Derived useState
+
+  const filteredProducts = products.filter((product) => {
+    const matchesSearch = product.name.toLowerCase().includes(q);
+    const matchesCategory = category === "all" || product.category === category;
+
+    return matchesSearch && matchesCategory;
+  });
   return (
     <section className="space-y-6 md:space-y-15 mx-auto max-w-6xl py-12 sm:py-16 px-4">
       <div className="text-center">
@@ -14,25 +28,18 @@ function Products() {
           Find the best stationery and laptop parts
         </p>
       </div>
-      <div className="flex sm:justify-between items-center  mx-auto flex-col md:flex-row gap-3 md:px-15 lg:px-0">
-        <div className="flex flex-wrap sm:flex-nowrap gap-3 ">
-          <Button variant="outline" size="xs">
-            ALL
-          </Button>
-          <Button variant="outline" size="xs">
-            STATIONERY
-          </Button>
-          <Button variant="outline" size="xs">
-            LAPTOP PARTS
-          </Button>
+      <ProductFilters />
+      {filteredProducts.length === 0 ? (
+        <p className="text-gray-500">
+          No products found matching your criteria.
+        </p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
-        <Input variant="search" size="sm" />
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:px-15 lg:px-0">
-        {products.map((product) => (
-          <ProductCard product={product} key={product.id} />
-        ))}
-      </div>
+      )}
     </section>
   );
 }
