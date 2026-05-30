48f435a59a41447a25b203999ce125ad40a0a484
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-05-30 11:25:00 +0300

    Improve empty cart layout styling

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -2,9 +2,9 @@ import Button from "../../ui/Button";
 
 export function EmptyCart() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center text-center text-primary text-2xl">
+    <div className="h-screen flex flex-col justify-center items-center text-center text-primary text-2xl gap-2">
       <p>!Opps looks like you havent added anything yet</p>
-      <Button variant="primary" to="/products" size="md">
+      <Button variant="primary" to="/products" size="md" className="rounded-sm">
         Continue Shopping
       </Button>
     </div>
