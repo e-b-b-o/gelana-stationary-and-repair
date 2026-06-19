94a54832ccda4c5e58d13ae3a3c9f4829255f8ab
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-06-19 22:56:12 +0300

    feat: add AppError import to product routes for error handling

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { getProducts, getProduct } from "../controllers/productController.js";
+import AppError from "../utils/AppError.js";
 
 const router = express.Router();
 
