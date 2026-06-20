e20edcb810986ca5f2a766ac945ccc7430efe4a9
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-06-20 21:29:27 +0300

    feat: implement product and product image validation schemas and add post route with validation middleware

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -2,11 +2,15 @@ import express from "express";
 
 import { getProducts, getProduct } from "../controllers/productController.js";
 import AppError from "../utils/AppError.js";
+import validationMiddleware from "../middleware/validationMiddleware.js";
+import { createProductSchema } from "../validation/product.schema.js";
 
 const router = express.Router();
 
 router.get("/", getProducts);
 
 router.get("/:id", getProduct);
+
+router.post("/", validationMiddleware(createProductSchema), createProduct);
 
 export default router;
