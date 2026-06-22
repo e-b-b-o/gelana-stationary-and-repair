359f53dcc7b782b434061c9fa5454b474365201a
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-06-23 01:54:31 +0300

    feat: implement user authentication, product categories, and product image management services and routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
+import categoriesRoutes from "./routes/categoriesRoutes.js";
+import productImageRoutes from "./routes/productImageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
 
 const app = express();
@@ -8,7 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/products/:productId/images", productImageRoutes);
+
+app.use("/api/categories", categoriesRoutes);
 
 app.use(errorMiddleware);
 
