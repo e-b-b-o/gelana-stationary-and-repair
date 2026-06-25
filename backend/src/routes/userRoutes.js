040884ed3d7b42b6e7792aa486973861131b6bf7
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-06-25 21:38:28 +0300

    fix(userRoutes): add logout route and update imports in userRoutes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { register, login, getProfile } from "../controllers/userController.js";
+import {
+  register,
+  login,
+  getProfile,
+  logout,
+} from "../controllers/userController.js";
 import validationMiddleware from "../middleware/validationMiddleware.js";
 import {
   registerUserSchema,
@@ -12,6 +17,7 @@ const router = express.Router();
 // Public routes
 router.post("/register", validationMiddleware(registerUserSchema), register);
 router.post("/login", validationMiddleware(loginUserSchema), login);
+router.post("/logout", logout);
 
 // Protected routes (You will add authMiddleware here later)
 router.get("/:id/profile", protect, getProfile);
