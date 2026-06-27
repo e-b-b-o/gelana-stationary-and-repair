015319e8f8c66c9737e3d32fad143d7494844f44
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-06-27 23:36:25 +0300

    feat(categories): add authentication and authorization for category creation, update, and deletion

--- backend/src/routes/categoriesRoutes.js
+++ backend/src/routes/categoriesRoutes.js
@@ -11,18 +11,30 @@ import {
   createCategorySchema,
   updateCategorySchema,
 } from "../validation/category.schema.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { restrictTo } from "../middleware/restrictTo.js";
 
 const router = express.Router();
 
 router
   .route("/")
   .get(getCategories)
-  .post(validationMiddleware(createCategorySchema), createCategory);
+  .post(
+    protect,
+    restrictTo("ADMIN"),
+    validationMiddleware(createCategorySchema),
+    createCategory,
+  );
 
 router
   .route("/:id")
   .get(getCategory)
-  .patch(validationMiddleware(updateCategorySchema), updateCategory)
-  .delete(deleteCategory);
+  .patch(
+    protect,
+    restrictTo("ADMIN"),
+    validationMiddleware(updateCategorySchema),
+    updateCategory,
+  )
+  .delete(protect, restrictTo("ADMIN"), deleteCategory);
 
 export default router;
