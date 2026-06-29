61cab198175b0352b64d0f6e9eb15c7a93f5b9f1
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-06-29 21:34:00 +0300

    refactor(main): simplify rendering by removing unnecessary context providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,21 +4,8 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
 
-import { AuthProvider } from "./features/auth/AuthContext";
-import { CartProvider } from "./features/cart/CartContext";
-import { WishlistProvider } from "./features/wishlist/WishlistContext";
-import { OrderProvider } from "./features/order/OrderContext";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <WishlistProvider>
-          <OrderProvider>
-            <RouterProvider router={router} />
-          </OrderProvider>
-        </WishlistProvider>
-      </CartProvider>
-    </AuthProvider>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
