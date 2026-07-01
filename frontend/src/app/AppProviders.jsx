f07abcfb501c78bf6ca0a9b4b110c5a76d4ebd83
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-07-01 22:37:43 +0300

    feat(provider): reorder providers to ensure AddressProvider wraps CartProvider

--- frontend/src/app/AppProviders.jsx
+++ frontend/src/app/AppProviders.jsx
@@ -3,17 +3,20 @@ import { AuthProvider } from "../features/auth/AuthContext";
 import { CartProvider } from "../features/cart/CartContext";
 import { WishlistProvider } from "../features/wishlist/WishlistContext";
 import { OrderProvider } from "../features/order/OrderContext";
+import { AddressProvider } from "../features/user/addresses/AddressContext";
 
 export default function AppProviders() {
   return (
     <AuthProvider>
-      <CartProvider>
-        <WishlistProvider>
-          <OrderProvider>
-            <Outlet />
-          </OrderProvider>
-        </WishlistProvider>
-      </CartProvider>
+      <AddressProvider>
+        <CartProvider>
+          <WishlistProvider>
+            <OrderProvider>
+              <Outlet />
+            </OrderProvider>
+          </WishlistProvider>
+        </CartProvider>
+      </AddressProvider>
     </AuthProvider>
   );
 }
